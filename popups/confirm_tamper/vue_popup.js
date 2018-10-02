cfe6e2667e84bd08c10ea772d3e8877bb72d7215
Carolina <carolina.jimenez.g@gmail.com>
2018-10-02 16:31:59 -0500

    Add vue.js

---
+++ popups/confirm_tamper/vue_popup.js
@@ -0,0 +1,15 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        table_items : [
+            {
+                type: 'uno',
+                description: 'uno'
+            },
+            {
+                type: 'dos',
+                description: 'dos'
+            },
+        ]
+    }
+})
